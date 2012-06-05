a93260b875a0e2231045d8bb22027ebf390ebe4b
Andrey Makhnach <andrey.makhnach@gmail.com>
2012-06-05 11:27:38 +0300

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,17 @@
 #! /usr/bin/env python
 """Script to install pluggable JQuery Mobile templates for Django
-
-pip install -e bzr+http://bazaar.launchpad.net/~mcfletch/django-jqm/trunk#egg=django-jqm
-
 """
-import os,sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
-version = [
-    (line.split('=')[1]).strip().strip('"').strip("'")
-    for line in open(os.path.join('jqm', '__init__.py'))
-    if line.startswith( '__version__' )
-][0]
-
-if __name__ == "__main__":
-    setup( 
-        name = 'django-jqm',
-        version = version,
-        description = 'Pluggable JQuery Mobile templates for Django',
-        author = 'Mike C. Fletcher',
-        author_email = 'mcfletch@vrplumber.com',
-        license = "MIT",
-        url = 'https://launchpad.net/django-jqm',
-        packages = ['jqm'],
-        package_dir = {
-            'jqm':'jqm',
-        },
-        options = {
-            'sdist':{
-                'force_manifest':1,
-                'formats':['gztar','zip'],},
-        },
-        zip_safe=False,
-        install_requires = [
-            'django >= 1.2',
-        ]
-    )
+setup(
+    name='django-jqm',
+    version='1.1.0',
+    description='Pluggable JQuery Mobile templates for Django',
+    author='Mike C. Fletcher',
+    author_email='mcfletch@vrplumber.com',
+    license="MIT",
+    url='https://launchpad.net/django-jqm',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
